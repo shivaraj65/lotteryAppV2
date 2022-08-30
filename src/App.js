150ba336515c25483c097427ed413549fc5ba04c
shivaraj <shivaraj.feb@gmail.com>
2022-08-30 20:44:03 +0530

    fixed redirect route

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
           <Route exact path="/login" element={<Login/>} />
           <Route exact path="/signup" element={<Signup/>} />
           <Route exact path="/dashboard/:id" element={<Dashboard/>} />
-          <Route exact path="mailauth" element={<Landing/>} >
-            <Route path="/mailauth/:id" element={<Redirecter/>} />
-          </Route>
+          {/* <Route exact path="mailauth" element={<Landing/>} > */}
+            <Route exact path="/mailauth/:id" element={<Redirecter/>} />
+          {/* </Route> */}
         
       </Routes>
     </HashRouter>
